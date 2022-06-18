5fa02cb43928d3a97770e0d6590d0951a1e23715
Patrick Coutinho <patrick@istyle.com.br>
2022-06-17 22:02:17 -0300

    chore: updated create-k8s-deployment.sh

--- sh/create-k8s-deployment.sh
+++ sh/create-k8s-deployment.sh
@@ -1,21 +1,28 @@
 #!/usr/bin/env bash
 
-export NEW_VERSION=$(git describe --tags --abbrev=0)
+NEW_VERSION=$(git describe --tags --abbrev=0)
+NEW_VERSION_NAME="${NEW_VERSION//./"-"}"
 echo NEW_VERSION: $NEW_VERSION
 
 aws eks update-kubeconfig --name cloud-devops-capstone
 
-kubectl apply -f k8s/backend-users-service.yaml
+SVC_ENV=green
+echo SVC_ENV: $SVC_ENV
 
-template=`cat "k8s/backend-users-deployment.yaml" | sed "s/{{NEW_VERSION}}/$NEW_VERSION/g"`
+template=`cat "k8s/backend-users-service.yaml" | sed "s/{{SVC_ENV}}/$SVC_ENV/g" | sed "s/{{NEW_VERSION_NAME}}/$NEW_VERSION_NAME/g"`
 echo "$template" | kubectl apply -f -
 
-kubectl apply -f k8s/backend-profiles-service.yaml
-
-template=`cat "k8s/backend-profiles-deployment.yaml" | sed "s/{{NEW_VERSION}}/$NEW_VERSION/g"`
+template=`cat "k8s/backend-users-deployment.yaml" | sed "s/{{NEW_VERSION}}/$NEW_VERSION/g" | sed "s/{{NEW_VERSION_NAME}}/$NEW_VERSION_NAME/g"`
 echo "$template" | kubectl apply -f -
 
-kubectl apply -f k8s/backend-bff-service.yaml
+template=`cat "k8s/backend-profiles-service.yaml" | sed "s/{{SVC_ENV}}/$SVC_ENV/g" | sed "s/{{NEW_VERSION_NAME}}/$NEW_VERSION_NAME/g"`
+echo "$template" | kubectl apply -f -
 
-template=`cat "k8s/backend-bff-deployment.yaml" | sed "s/{{NEW_VERSION}}/$NEW_VERSION/g"`
+template=`cat "k8s/backend-profiles-deployment.yaml" | sed "s/{{NEW_VERSION}}/$NEW_VERSION/g" | sed "s/{{NEW_VERSION_NAME}}/$NEW_VERSION_NAME/g"`
+echo "$template" | kubectl apply -f -
+
+template=`cat "k8s/backend-bff-service.yaml" | sed "s/{{SVC_ENV}}/$SVC_ENV/g" | sed "s/{{NEW_VERSION_NAME}}/$NEW_VERSION_NAME/g"`
+echo "$template" | kubectl apply -f -
+
+template=`cat "k8s/backend-bff-deployment.yaml" | sed "s/{{NEW_VERSION}}/$NEW_VERSION/g" | sed "s/{{NEW_VERSION_NAME}}/$NEW_VERSION_NAME/g"`
 echo "$template" | kubectl apply -f -
