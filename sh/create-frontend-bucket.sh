4c73cd95deba98af0bfae278ac0ac9198baf53b3
Patrick Coutinho <patrick@istyle.com.br>
2022-06-22 19:37:13 -0300

    ci: updated frontend steps

--- sh/create-frontend-bucket.sh
+++ sh/create-frontend-bucket.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
 export NEW_VERSION=$(git describe --tags --abbrev=0)
+NEW_VERSION_NAME="${NEW_VERSION//./"-"}"
 echo NEW_VERSION: $NEW_VERSION
 
 aws cloudformation deploy \
     --template-file ./cloudformation/frontend/s3.yml \
-    --stack-name "cloud-devops-capstone-${NEW_VERSION}" \
+    --stack-name "cloud-devops-capstone-${NEW_VERSION_NAME}" \
     --tags project=cloud-devops-capstone \
-    --parameter-overrides version="${NEW_VERSION}"
+    --parameter-overrides version="${NEW_VERSION_NAME}"
--- sh/update-cloudfront.sh
+++ sh/update-cloudfront.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
 export NEW_VERSION=$(git describe --tags --abbrev=0)
+NEW_VERSION_NAME="${NEW_VERSION//./"-"}"
 echo NEW_VERSION: $NEW_VERSION
 
 aws cloudformation deploy \
     --template-file ./cloudformation/frontend/cloudfront.yml \
     --stack-name CloudDevopsCapstoneFront \
-    --parameter-overrides version="${NEW_VERSION}" \
+    --parameter-overrides version="${NEW_VERSION_NAME}" \
     --tags project=cloud-devops-capstone
