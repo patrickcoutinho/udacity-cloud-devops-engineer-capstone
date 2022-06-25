7fb158b6dd34a04e062ab1503fa359e6a9d32384
Patrick Coutinho <patrick@istyle.com.br>
2022-06-25 09:07:22 -0300

    feat: some frontend changes

--- frontend/components/table/table.tsx
+++ frontend/components/table/table.tsx
@@ -3,13 +3,13 @@ const Table = ({ values }) => {
     <table className="border-collapse table-fixed w-full text-sm bg-slate-800">
       <thead>
         <tr>
-          <th className="border-b dark:border-slate-600 font-medium p-4 pl-8 pt-3 pb-3 text-slate-200 text-left">
+          <th className="border-b w-40 dark:border-slate-600 font-medium p-4 pt-3 pb-3 text-slate-200 text-left">
             ID
           </th>
-          <th className="border-b dark:border-slate-600 font-medium p-4 pl-8 pt-3 pb-3 text-slate-200 text-left">
+          <th className="border-b dark:border-slate-600 font-medium p-4 pt-3 pb-3 text-slate-200 text-left">
             Login
           </th>
-          <th className="border-b dark:border-slate-600 font-medium p-4 pl-8 pt-3 pb-3 text-slate-200 text-left">
+          <th className="border-b dark:border-slate-600 font-medium p-4 pt-3 pb-3 text-slate-200 text-left">
             Name
           </th>
         </tr>
@@ -18,13 +18,13 @@ const Table = ({ values }) => {
         {values.length &&
           values.map((item, key) => (
             <tr key={key}>
-              <td className="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-400">
+              <td className="border-b border-slate-100 dark:border-slate-700 p-4 text-slate-400">
                 {item.id}
               </td>
-              <td className="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-400">
+              <td className="border-b border-slate-100 dark:border-slate-700 p-4 text-slate-400">
                 {item.login}
               </td>
-              <td className="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-400">
+              <td className="border-b border-slate-100 dark:border-slate-700 p-4 text-slate-400">
                 {item.name}
               </td>
             </tr>
